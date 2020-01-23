88d31cc36bec84859c7446240fad221ae4163840
zoltan23 <timothy_tassin@hotmail.com>
2020-01-23 01:42:38 -0700

    added scripts

---
+++ transform_image_resolution.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+import argparse
+
+def rescale_images(directory, size):
+    for img in os.listdir(directory):
+        im = Image.open(directory+img)
+        im_resized = im.resize(size, Image.ANTIALIAS)
+        im_resized.save(directory+img)
+
+def main():
+    for folder in ['train', 'test']:
+        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv(('images/'+folder+'_labels.csv'), index=None)
+print('Successfully converted xml to csv.')
